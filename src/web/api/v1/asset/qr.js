9513a9ae86b089ceef27d4c6968b8cd05a6c4a3c
Sebastian Kubalski <s.kubalski@polsource.com>
2018-07-18 14:12:04 +0200

    fix

--- src/web/api/v1/asset/qr.js
+++ src/web/api/v1/asset/qr.js
@@ -3,13 +3,13 @@ const pgDatabase = require('../../../../lib/pg-database');
 const S3Helper = require('../../../../lib/s3-helper');
 const QrCodeHelper = require('../../../../lib/qr-code-helper');
 const _ = require('lodash');
-const {NotFoundError, UnexpectedError} = require('../../../../lib/errors');
+const {NotFoundError, UnexpectedError, InvalidRequestError} = require('../../../../lib/errors');
 const {sfidValidator} = require('../../../../lib/validators');
 
 app.get('/:id', sfidValidator, async (req, res, next) => {
   try {
     const qrCode = await pgDatabase.task(
-        t => t.oneOrNone('SELECT s3_object_name FROM a18.qr_code WHERE asset = $[assetId]'), {assetId: req.params.id}
+        t => t.oneOrNone('SELECT s3_object_name FROM a18.qr_code WHERE asset = $[assetId]', {assetId: req.params.id})
     );
 
     if (_.isNil(qrCode)) {
@@ -35,20 +35,27 @@ app.post('/:id', sfidValidator, async (req, res, next) => {
           'select * from salesforce.asset__c where sfid = $[assetId]',
           {assetId: req.params.id}
       );
-
       if (_.isNil(asset)) {
         next(new NotFoundError());
       } else {
-        const qrCodeImage = QrCodeHelper.create(asset);
-
-        const s3Helper = new S3Helper();
-        s3Helper.connect();
-        const name = await s3Helper.upload(qrCodeImage);
-        await t.none(
-            'INSERT INTO a18.qr_code(asset, s3_object_name) VALUES($[assetId], $[name])',
-            {name, assetId: req.params.id}
+        const qrCode = await t.oneOrNone(
+            'SELECT s3_object_name FROM a18.qr_code WHERE asset = $[assetId]',
+            {assetId: req.params.id}
         );
-        res.sendStatus(201);
+        if (_.isNil(qrCode)) {
+          next(new InvalidRequestError());
+        } else  {
+          const qrCodeImage = QrCodeHelper.create(asset);
+
+          const s3Helper = new S3Helper();
+          s3Helper.connect();
+          const name = await s3Helper.upload(qrCodeImage);
+          await t.none(
+              'INSERT INTO a18.qr_code(asset, s3_object_name) VALUES($[assetId], $[name])',
+              {name, assetId: req.params.id}
+          );
+          res.sendStatus(201);
+        }
       }
     });
   } catch (err) {
@@ -56,7 +63,7 @@ app.post('/:id', sfidValidator, async (req, res, next) => {
   }
 });
 
-app.put('/:assetId', sfidValidator, async (req, res, next) => {
+app.put('/:id', sfidValidator, async (req, res, next) => {
   try {
     await pgDatabase.task(async t => {
       const asset = await t.oneOrNone(
@@ -76,7 +83,7 @@ app.put('/:assetId', sfidValidator, async (req, res, next) => {
         const s3Helper = new S3Helper();
         s3Helper.connect();
         await s3Helper.upload(qrCodeImage, qrCode.s3_object_name);
-        res.sendStatus(201);
+        res.sendStatus(200);
       }
     });
   } catch (err) {
