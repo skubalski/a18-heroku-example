b4a8873ee0d8b71edc74f24b8ca4b44f5e7d9338
Sebastian Kubalski <s.kubalski@polsource.com>
2018-07-18 14:04:12 +0200

    fix

--- src/lib/validators.js
+++ src/lib/validators.js
@@ -1,10 +1,10 @@
 const {InvalidRequestError} = require('./errors');
 
 function sfidValidator(req, res, next) {
-  if (req.params && req.params.id && (req.params.id.length === 15 || req.params.id.length === 18)) {
-    next(new InvalidRequestError());
-  } else {
+  if (req.params && req.params.id && req.params.id.length === 18) {
     next();
+  } else {
+    next(new InvalidRequestError());
   }
 }
 
